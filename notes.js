8b1b47a519d9ba116e08be7f9a1550bf99199804
Safaa Abukar <abukarsafa@gmail.com>
2021-03-05 17:02:15 +0000

    update

--- notes.js
+++ notes.js
@@ -1,1 +1,8 @@
-console.log('Hi')
+console.log(5);
+// console.log() logs or prints onto the screen
+
+// this is a single line comment
+/* this is a 
+multiline comment */
+console.log( /* but this can also be used inside of brackets like this */ 22)
+    //so the comment will be ignored
